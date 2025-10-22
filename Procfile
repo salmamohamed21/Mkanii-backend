b76cb134cf0394f1e894128f5a320f04b47d2f66
salmamohamed21 <salmamohamed0266@gmail.com>
2025-10-22 17:09:28 +0300

    Update Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn mkani.wsgi:application --bind 0.0.0.0:$PORT --workers 3 --chdir mkani
+web: gunicorn mkani.wsgi:application --bind 0.0.0.0:$PORT --workers 3 --chdir .
--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ set -o errexit  # stop the script if any command fails
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-gunicorn mkani.wsgi:application --bind 0.0.0.0:$PORT --workers 3 --chdir mkani
+gunicorn mkani.wsgi:application --bind 0.0.0.0:$PORT --workers 3 --chdir .
